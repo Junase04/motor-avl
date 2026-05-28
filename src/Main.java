abce957dab7f63455bbbbf36fa7c997a64261167
Junase04 <jpgiraldob@udistrital.edu.co>
2026-05-28 14:44:09 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Inicialización del gestor de almacenamiento central
+        // Inicialización del gestor de almacenamiento 
         DatabaseManager dbManager = new DatabaseManager();
 
         // Inyección de dependencias en el componente de interfaz de usuario
